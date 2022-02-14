4f28fdd776408094cef6df444ade28e828e3a6e4
Insa <ilya2002i52@mail.ru>
2022-02-14 23:33:40 +0500

    adoptation

--- js/script.js
+++ js/script.js
@@ -5,11 +5,16 @@
 class Sample {
     constructor(props) {
         this.parent = document.querySelector('.ex__box');
-        this.amount = props.amount;
+        this.amount = +props.amount;
         this.numArray = ['+', '-', '*', '/'];
         this.score = 0;
         this.counter = 0;
-
+        this.phrases = {
+            excellent: 'Это Великолепно!!!',
+            good: 'Очень хороший результат)',
+            normal: 'Не плохо)',
+            bad: 'Тебе стоит больше решать'
+        };
         this.render();
 
 
@@ -18,11 +23,11 @@ class Sample {
     render() {
 
         for (let i = 0; i < this.amount; i++) {
-           
+
             let firstNum = this.getRandomNum(0, 10);
             let secondNum = this.getRandomNum(0, 10);
             let randomSign = this.getRandomSign();
-            let sampleStr = `${String(firstNum)} ${String(randomSign)} ${String(secondNum)}`;
+            let sampleStr = `${String(firstNum)} ${String(randomSign)} ${String(secondNum)} =`;
 
             let sampleResult;
             if (randomSign === '+') {
@@ -35,11 +40,13 @@ class Sample {
             sample.classList.add('sample');
 
             let pattern = `
+                <div class="content">
                     <div class="sample__str">
                         ${sampleStr}
                     </div>
-                    <input type="text" class="sample__input">
-                    <div class="sample__result hidden">
+                    <input type="text" class="sample__input">     
+                </div>
+                <div class="sample__result hidden">
                         ${sampleResult}
                     </div>
                     <input type="submit" class="sample__check" value="Проверить">
@@ -58,7 +65,7 @@ class Sample {
                 if (input.value == sampleResult) {
                     input.style.color = '#4dfb4d';
                     this.score += 1;
-                    
+
 
                 } else if (input.value == '') {
                     input.style.color = 'red';
@@ -71,13 +78,22 @@ class Sample {
 
                 }
                 this.counter += 1;
-                console.log(this.counter);
-                console.log(this.amount);
-                if(this.counter == this.amount){
-                    console.log('i here');
-                    this.showResult();
+                if (this.counter == this.amount) {
+
+                    const percent = Math.floor(this.score * 100 / this.amount);
+                    console.log(percent);
+                    if (percent == 100) {
+                        this.showResult(this.phrases.excellent, percent);
+                    } else if (percent >= 60 && percent < 100) {
+                        this.showResult(this.phrases.good, percent);
+                    } else if (percent >= 30 && percent < 60) {
+                        this.showResult(this.phrases.normal, percent);
+                    } else if (percent >= 30 && percent < 60) {
+                        this.showResult(this.phrases.bad, percent);
+                    }
+
                 }
-                
+
             });
         }
     }
@@ -92,14 +108,16 @@ class Sample {
         return this.numArray[this.getRandomNum(0, 1)];
     }
 
-    showResult(){
+    showResult(text, percent) {
         let modalResult = document.createElement('div');
 
         modalResult.classList.add('result');
 
         modalResult.innerHTML = `
         <div class="result__box">
-        <p class="result__text">У тебя ${this.score} очков)</p>
+        <p class="result__text">${text},<br> У тебя ${percent}% правильных
+        <br> ${this.score}/${this.amount}
+        </p>
         <input type="submit" class="result__closer" value="Еще разок?)">
         </div>
         `;
@@ -107,7 +125,7 @@ class Sample {
         document.body.append(modalResult);
 
 
-        document.querySelector('.result__closer').addEventListener('click', ()=>{
+        document.querySelector('.result__closer').addEventListener('click', () => {
             document.location.reload();
         });
     }
